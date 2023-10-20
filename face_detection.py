b2dce9a905fb0e892b598905430c5f5ba79a5138
Divya sri Srikakulapu <84381735+20A91A04B4@users.noreply.github.com>
2023-10-20 14:58:32 +0530

    Update face_detection.py

--- face_detection.py
+++ face_detection.py
@@ -4,7 +4,7 @@
 import cv2
 
 face_cascade = cv2.CascadeClassifier(r"C:\Users\91630\Downloads\haarcascade_frontalface_default.xml")
-nadia = cv2.imread(r"C:\Users\91630\Downloads\WhatsApp Image 2023-03-01 at 2.12.45 PM.jpeg",0)
+pic = cv2.imread(r"C:\Users\91630\Downloads\WhatsApp Image 2023-03-01 at 2.12.45 PM.jpeg",0)
 
 def detect_face(img):
     face_img = img.copy()
@@ -14,7 +14,7 @@ def detect_face(img):
         cv2.rectangle(face_img,(x,y),(x+w,y+h),(255,255,255),10)
     return face_img
 
-result = detect_face(nadia)
+result = detect_face(pic)
 
 while True:
     cv2.imshow('',result)
